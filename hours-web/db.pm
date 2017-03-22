5876416e71c3e9bc82101d1fa3fb291372cc73ce
Oscar Buse <oscar@kwalinux.nl>
2017-03-22 09:26:47 +0100

    little changes

--- hours-web/db.pm
+++ hours-web/db.pm
@@ -2,18 +2,15 @@ package db;
 
 require Exporter;
 @ISA = qw(Exporter);
-#@EXPORT = qw($hostname $owner $master $server $port $username $password $dbh);
 @EXPORT = qw($dbh);
 
 # initialize package globals, first exported ones
 $hostname = "localhost";
-$owner = "oscar\@all-stars.nl";
-$master = "oscar\@all-stars.nl";
-$database = "accounting";		# Name of the MySQL database
+$database = "accounting";
 $server = "172.18.0.2";
-$port = "3306";			# Port on which MySQL receives input
-$username = "uren";		# Username for MySQL login
-$password = "registratie";	# Password for MySQL login
+$port = "3306";
+$username = "alice";		# Username for MySQL login
+$password = "changeme";		# Password for MySQL login
 
 # connect with database
 $dbh = DBI->connect("DBI:mysql:$database:$server:$port",$username,$password);
